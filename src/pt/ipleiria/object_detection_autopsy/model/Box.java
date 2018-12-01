c74c362517036b453bd981473748adb6da89a702
Ruben Caceiro <caceiro.ruben@gmail.com>
2018-12-01 11:00:52 +0000

    Created Box model

--- src/pt/ipleiria/object_detection_autopsy/model/Box.java
+++ src/pt/ipleiria/object_detection_autopsy/model/Box.java
@@ -1,15 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pt.ipleiria.object_detection_autopsy.model;
 
-/**
- *
- * @author rcaceiro
- */
 public class Box
 {
- 
+ private double x;
+ private double y;
+ private double width;
+ private double height;
+
+ public Box()
+ {
+  this(0, 0, 0, 0);
+ }
+
+ public Box(double x, double y, double width, double height)
+ {
+  super();
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+ }
+
+ public double getX()
+ {
+  return x;
+ }
+
+ public double getY()
+ {
+  return y;
+ }
+
+ public double getWidth()
+ {
+  return width;
+ }
+
+ public double getHeight()
+ {
+  return height;
+ }
 }
