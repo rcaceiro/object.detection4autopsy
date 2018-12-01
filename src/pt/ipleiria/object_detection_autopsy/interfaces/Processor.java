3a3e568ea7905064afa4a0a6b98a55a594cc517b
Ruben Caceiro <caceiro.ruben@gmail.com>
2018-12-01 11:25:41 +0000

    Created Processor interfac

--- src/pt/ipleiria/object_detection_autopsy/interfaces/Processor.java
+++ src/pt/ipleiria/object_detection_autopsy/interfaces/Processor.java
@@ -1,6 +1,11 @@
 package pt.ipleiria.object_detection_autopsy.interfaces;
 
+import pt.ipleiria.object_detection_autopsy.model.ImageDetection;
+import pt.ipleiria.object_detection_autopsy.model.VideoDetection;
+
 public interface Processor
 {
- public void processImage(String path);
+ public ImageDetection processImage(String path);
+ public VideoDetection processVideo(String path);
+ public boolean isProcessorAvailable();
 }
